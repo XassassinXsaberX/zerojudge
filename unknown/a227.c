872f277fe51b6a8804c320a377d8f5f6747aa75f
a5083 <a5083@study.centos.vbird>
2017-06-27 20:56:58 +0800

    完成a227河內塔

---
+++ unknown/a227.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void recursion(char start,char mid,char end,int ring)
+{
+	if(ring==1)   
+		printf("Move ring %d from %c to %c\n",ring,start,end);
+	else
+	{	
+
+		recursion(start,end,mid,ring-1);
+		printf("Move ring %d from %c to %c\n",ring,start,end);
+		recursion(mid,start,end,ring-1);
+	}
+
+	
+}
+
+int main()
+{
+	int N;
+	while(scanf("%d",&N)!=EOF)
+	{
+		recursion('A','B','C',N);
+
+		printf("\n");
+	}
+	return 0;
+}
+
