ee53fd7914846aa0c14c3e62bde7bbaff9db96b6
PMKIONMUO\user <a5083a5083@gmail.com>
2017-08-10 18:59:51 +0800

    更新merge sort中"合併"的程式寫法(更直觀易讀)

--- normal/a104(merge sort).c
+++ normal/a104(merge sort).c
@@ -55,23 +55,16 @@ void merge_sort(int*arr,int *temp,int n)
 	i = 0,j = mid,k = 0;
 	while(1)
 	{
-		for(;i<mid;i++)
-		{
-			if(arr[i]<=arr[j] || j>=n)  // 若第一堆的某一個元素<=第二堆，或是第二堆的元素已全部丟到temp陣列中的情況時 
-				temp[k++] = arr[i];	    // 則將第一堆的該元素丟到temp陣列中
-			else
-				break;
-		}
-		for(;j<n;j++)
-		{
-			if(arr[j]<=arr[i] || i>=mid) // 若第二堆的某一個元素<=第一堆，或是第一堆的元素已全部丟到temp陣列中的情況時 
-				temp[k++] = arr[j];      // 則將第二堆的該元素丟到temp陣列中 
-			else
-				break;
-		}
-		if(k == n)                       // 若temp陣列收到了n個元素，代表第一堆及第二堆的所有元素都丟到temp陣列中了 
-			break;
-				
+		if(i>=mid && j>=n)  //如果第一堆和第二堆的元素都已經丟到temp陣列中 
+			break;          //就跳出迴圈 
+		if(i<mid && j<n && arr[i]<=arr[j])       //若第一堆和第二堆的元素還沒全部丟到temp陣列中，且目前第一堆的元素arr[i]<=目前第二堆的元素arr[j] 
+			temp[k++] = arr[i++];                //將目前第一堆的前面元素丟到temp陣列中 
+		else if(i<mid && j<n && arr[j]<=arr[i])  //若第一堆和第二堆的元素還沒全部丟到temp陣列中，且目前第二堆的元素arr[j]<=目前第一堆的元素arr[i]  
+			temp[k++] = arr[j++];                //將目前第二堆的前面元素丟到temp陣列中 
+		else if(i<mid && j>=n)                   //若第一堆的元素還沒全部丟到temp陣列中，但第二堆的元素已全部丟到temp陣列中 
+			temp[k++] = arr[i++];                //將目前第一堆的前面元素丟到temp陣列中 
+		else if(i>=mid && j<n)                   //若第二堆的元素還沒全部丟到temp陣列中，但第一堆的元素已全部丟到temp陣列中  
+			temp[k++] =arr[j++];                 //將目前第二堆的前面元素丟到temp陣列中 
 	}
 	
 	//現在temp陣列中都是已排序好的元素，將temp陣列中的元素複製到arr陣列中吧 
